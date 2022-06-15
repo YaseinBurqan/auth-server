b6385a7c7f35f9a481a34f9d130fb171c34d8f39
Yasein Burqan <mr.yasein@gmail.com>
2022-06-15 22:15:40 +0300

    bearer-auth

---
+++ src/server.js
@@ -0,0 +1,44 @@
+"use strict";
+
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const notFoundHandler = require("./error-handlers/404");
+const errorHandler = require("./error-handlers/500");
+
+const signInRoutes = require("./routes/signIn");
+const signUpRoutes = require("./routes/signUp");
+
+const secretRouter = require("./routes/secretRouter");
+const allUsersRouter = require("./routes/allUsersRouter");
+
+
+const logger = require("./middleware/logger");
+
+app.get("/", (req, res) => {
+  res.status(200).send("server is working");
+});
+
+app.use(signInRoutes);
+app.use(signUpRoutes);
+
+app.use(secretRouter);
+app.use(allUsersRouter);
+
+app.use("*", notFoundHandler);
+app.use(errorHandler);
+
+function start(PORT) {
+  app.listen(PORT, () => {
+    console.log(`Listen on port ${PORT}`);
+  });
+}
+
+module.exports = {
+  app: app,
+  start: start,
+};
